a537d8f87873049703d75c971c193f54774684a1
MartaCollPol <maly-cat@hotmail.es>
2023-07-03 19:36:29 +0200

    Created bc module, updated functions after testing.
     Changes to be committed:
    	new file:   binary_classification/__init__.py
    	new file:   binary_classification/__pycache__/__init__.cpython-39.pyc
    	new file:   binary_classification/__pycache__/models.cpython-39.pyc
    	new file:   binary_classification/__pycache__/tools.cpython-39.pyc
    	renamed:    src/models.py -> binary_classification/models.py
    	renamed:    src/binary_classification.py -> binary_classification/tools.py
    	modified:   data/clean_breast_cancer.csv
    	new file:   notebooks/.ipynb_checkpoints/Model_Evaluation-checkpoint.ipynb
    	new file:   notebooks/Model_Evaluation.ipynb
    	new file:   notebooks/data/outputs/LogisticRegression/Test_Evaluation.csv
    	new file:   notebooks/data/outputs/LogisticRegression/Train_Evaluation.csv
    	new file:   notebooks/data/outputs/LogisticRegression/model.pkl
    	modified:   notebooks/data_preprocessing.ipynb
    	modified:   requirements.txt

---
+++ binary_classification/tools.py
@@ -0,0 +1,148 @@
+"""
+Implements all methods for training, optimization, evaluation and prediction
+using ML models defined in Model for binary classification with numerical attributes.
+"""
+import os
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve
+from sklearn import metrics
+
+
+def load_data(data_path):
+    """
+    Loads cleaned data from csv. Returns pandas DataFrame.
+    """
+    assert os.path.exists(data_path), "File does not exist"
+    assert os.path.splitext(data_path)[1] == ".csv", "File is not a CSV"
+
+    df = pd.read_csv(data_path)
+
+    return df
+
+
+def split_train_test(df, predictors, target, test_size = 0.2):
+    """
+    Splits data in train, test and val sets. Returns dict of dataframes. 
+    """
+
+    assert target in df.columns.tolist(), 'Target {self._target} not present in df.'
+    assert all(elem in df.columns.tolist() for elem in predictors), (f'Provided ' + 
+                                        'predictors {predictors} not present in df.')
+
+    X = df[predictors] # Features.
+    y = df[target] # Target variable.
+
+    # Split the data into train and test sets with shuffling.
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size,
+                                                        shuffle=True, random_state=42)
+
+    return {'X_train': X_train, 'X_test': X_test,
+            'y_train': y_train, 'y_test': y_test}
+
+
+def plot_learning_curve(model_obj, X, y, cv=None,
+                        train_sizes=np.linspace(0.1, 1.0, 10)):
+    """
+    Plots learning curve of a model training to asses if it is
+    underfitting or overfitting using cross-validation.
+    """
+    model = model_obj.model
+
+    train_sizes, train_scores, val_scores = learning_curve(model,
+                                                           X,
+                                                           y,
+                                                           cv=cv,
+                                                           train_sizes=train_sizes)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    val_scores_mean = np.mean(val_scores, axis=1)
+    val_scores_std = np.std(val_scores, axis=1)
+
+    plt.figure(figsize=(8, 6))
+    plt.title('Learning Curve')
+    plt.xlabel('Training Examples')
+    plt.ylabel('Score')
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1, color='r')
+    plt.fill_between(train_sizes, val_scores_mean - val_scores_std,
+                     val_scores_mean + val_scores_std, alpha=0.1, color='g')
+    plt.plot(train_sizes, train_scores_mean, 'o-', color='r', label='Training Score')
+    plt.plot(train_sizes, val_scores_mean, 'o-', color='g', label='Validation Score')
+
+    plt.legend(loc='best')
+    plt.show()
+
+
+def evaluation(y_true, y_pred):
+    """
+    Given target real values and predicted ones by a model, this function
+    calculates different classification metrics and return them in a dict.
+    """
+    results = {}
+
+    results['accuracy'] = [round(metrics.accuracy_score(y_true, y_pred), 4)]
+    results['precision'] = [round(metrics.precision_score(y_true, y_pred), 4)]
+    results['recall'] = [round(metrics.recall_score(y_true, y_pred), 4)]
+    results['f1'] = [round(metrics.f1_score(y_true, y_pred), 4)]
+    results['auc_roc'] = [round(metrics.roc_auc_score(y_true, y_pred), 4)]
+
+    return pd.DataFrame(results)
+
+
+def train_and_evaluate_model(model_obj, data, store_outputs=False):
+    """
+    Function to train model and store its weights. 
+    """
+
+    model = model_obj.model
+    model.fit(data['X_train'], data['y_train'])
+
+    preds_train = model.predict(data['X_train'])
+    preds_test = model.predict(data['X_test'])
+
+    eval_train = evaluation(data['y_train'], preds_train)
+    eval_test = evaluation(data['y_test'], preds_test)
+
+    print(f"For model {model_obj.model_name} with {model_obj.model_hyperparameters}:\n")
+    print("Results for train set:\n")
+    print(eval_train)
+
+    print("\nResults for test set:\n")
+    print(eval_test)
+
+    confusion_matrix = metrics.confusion_matrix(data['y_test'], preds_test)
+    class_labels = ['Class 0', 'Class 1']
+
+    # Create a heatmap plot of the confusion matrix
+    sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues',
+                xticklabels=class_labels, yticklabels=class_labels)
+
+    # Add labels, title, and ticks
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix for Test set')
+    plt.xticks(np.arange(len(class_labels)), class_labels)
+    plt.yticks(np.arange(len(class_labels)), class_labels)
+
+    if store_outputs:
+        # Create directories if they don't exist
+        output_dir = f'data/outputs/{model_obj.model_name}'
+        os.makedirs(output_dir, exist_ok=True)
+        # Save the model
+        with open(f'{output_dir}/model.pkl', 'wb') as file:
+            pickle.dump(model, file)
+
+        # Save the evaluation
+        eval_train.to_csv(f'data/outputs/{model_obj.model_name}/Train_Evaluation.csv')
+        eval_test.to_csv(f'data/outputs/{model_obj.model_name}/Test_Evaluation.csv')
+
+# TODO: hyperparam optimization method
