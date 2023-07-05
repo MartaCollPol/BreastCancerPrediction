37dd9e3c8ac87ac2bef2c10252537990de815c39
MartaCollPol <maly-cat@hotmail.es>
2023-07-05 09:59:22 +0200

    adding utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
@@ -46,3 +48,37 @@ def save_dict_to_csv(data_dict, data_path):
       writer.writeheader()
       writer.writerow(data_dict)
 
+
+def save_model_outputs(model, model_name, hyperparams, eval_train, eval_test, output_dir):
+    """
+    Saves model to a pickle, model hyperparams and evaluation results to output_dir.
+    """
+    # Create directories if they don't exist.
+    model_dir = output_dir + '/' + model_name
+    os.makedirs(model_dir, exist_ok=True)
+
+    # Save the model.
+    with open(f'{model_dir}/model.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+    # Save best params.
+    utils.save_dict_to_csv(hyperparams, f'{model_dir}/params.csv' )
+
+    # Save the evaluation.
+    eval_train.to_csv(f'{model_dir}/Train_Evaluation.csv')
+    eval_test.to_csv(f'{model_dir}/Test_Evaluation.csv')
+
+
+def plot_confusion_matrix(confusion_matrix, class_labels):
+    """
+    Plots a confusion matrix using a headmap.
+    """
+    sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues',
+                xticklabels=class_labels, yticklabels=class_labels)
+
+    # Add labels, title, and ticks
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix for Test set')
+    plt.xticks(np.arange(len(class_labels)), class_labels)
+    plt.yticks(np.arange(len(class_labels)), class_labels)
